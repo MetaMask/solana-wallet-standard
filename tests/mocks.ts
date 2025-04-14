45af634607ac93f2605a115808a64217657b6578
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-04-14 16:30:46 +0200

    Integration tests

---
+++ tests/mocks.ts
@@ -0,0 +1,53 @@
+import { SOLANA_MAINNET_CHAIN } from '@solana/wallet-standard-chains';
+import bs58 from 'bs58';
+import { vi } from 'vitest';
+
+export const mockAddress = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+export const mockPublicKey = bs58.decode(mockAddress);
+export const mockScope = 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp';
+export const mockChain = SOLANA_MAINNET_CHAIN;
+
+// Create mock for MultichainApiClient
+export const createMockClient = () => {
+  return {
+    onNotification: vi.fn(),
+    getSession: vi.fn(),
+    createSession: vi.fn(),
+    invokeMethod: vi.fn(),
+    revokeSession: vi.fn(),
+    extendsRpcApi: vi.fn(),
+  };
+};
+
+// Helper to setup a session with an account
+export const mockGetSession = (mockClient: ReturnType<typeof createMockClient>, address?: string) => {
+  mockClient.getSession.mockResolvedValue({
+    sessionScopes: address
+      ? {
+          [mockScope]: {
+            accounts: [`${mockScope}:${mockAddress}`],
+          },
+        }
+      : {},
+  });
+};
+
+export const mockCreateSession = (mockClient: ReturnType<typeof createMockClient>, address?: string) => {
+  mockClient.createSession.mockResolvedValue({
+    sessionScopes: address
+      ? {
+          [mockScope]: {
+            accounts: [`${mockScope}:${mockAddress}`],
+          },
+        }
+      : {},
+  });
+};
+
+// Mock window object for tests
+// @ts-ignore - Mocking window
+global.window = {
+  location: {
+    host: 'example.com',
+  } as Location,
+};
