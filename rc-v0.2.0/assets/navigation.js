81d0b4cd2c7261d65c56f187ccc43c1dc280f8f2
EdouardBougon <EdouardBougon@users.noreply.github.com>
2025-04-23 20:11:47 +0000

    deploy: 244c2c1748619978f6166d04904200c1ab7e9b44

---
+++ rc-v0.2.0/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKTy0JT8zJSS0JLknMS0ksSlHSUSpILMlQslJKK81LLsnMzyvWx1Ckl1GSm6Oko5SdmZeiZGVmUqsDN7AoNT2zuCS1KDg/JzEvkbDZ+NRjWBMLAPdP24O5AAAA"
